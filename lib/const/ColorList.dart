2ebc4c2d797ff78cf8e8aa6d565da8763be0e7e3
JunGyu0868 <rlawnsrb3@nate.com>
2021-08-03 23:19:37 +0900

    setting init and folder

---
+++ lib/const/ColorList.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kBackgroudnColor = Color(0xFFF8F8F8);
+const kActiveIconColor = Color(0xFFE68342);
+const kTextColor = Color(0xFF222B45);
+const kBlueLigtColor = Color(0xFF817DC0);
+const kBlueColor = Color(0xFF817DC0);
+const kShadowColor = Color(0xFFE6E6E6);
